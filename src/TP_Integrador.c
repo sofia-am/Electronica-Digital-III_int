cc3864a5f85f229d3783812f9990870baac1b820
akmsw <franbonino82@gmail.com>
2021-10-20 17:45:46 -0300

    prueba

--- src/TP_Integrador.c
+++ src/TP_Integrador.c
@@ -1,35 +1,17 @@
 /*
 ===============================================================================
- Name        : TP_Integrador.c
- Author      : $(author)
- Version     :
- Copyright   : $(copyright)
- Description : main definition
+ Nombre        : TP_Integrador.c
+ Autores      : Amallo, Sofía; Covacich, Axel; Bonino Francisco Ignacio
+ Version     : 1.0
+ Copyright   : None
+ Description : TP final para Electrónica Digital III
 ===============================================================================
 */
 
-#ifdef __USE_CMSIS
-#include "LPC17xx.h"
-#endif
-
-#include <cr_section_macros.h>
-
-// TODO: insert other include files here
-
-// TODO: insert other definitions and declarations here
+#include "lpc17xx.h"
 
 int main(void) {
 
-    // TODO: insert code here
-
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-        // "Dummy" NOP to allow source level single
-        // stepping of tight while() loop
-        __asm volatile ("nop");
-    }
+    // saranic rules
     return 0 ;
 }
