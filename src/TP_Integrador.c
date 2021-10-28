85e20bc12584433a3f6be56d51da6f5c5517f94f
Sofia A <sofiamallo07@hotmail.com>
2021-10-28 12:34:53 -0300

    capture, falta DMA

--- src/TP_Integrador.c
+++ src/TP_Integrador.c
@@ -9,6 +9,7 @@
 */
 
 #include "lpc17xx.h"
+#include "lpc17xx_timer.h"
 
 void configUART();
 void configCapture();
@@ -23,5 +24,34 @@ int main(void) {
 }
 
 void configCapture(){
+	//100Mhz por default
+	//queremos que el timer funcione con una precision de 0,1 seg
+	//configurar el prescaler para 0.1 seg = 100.000 useg
+
+	TIM_TIMERCFG_Type config;
+	//TIM_COUNTERCFG_Type config_counter;
+	TIM_CAPTURECFG_Type config_capture;
+
+	config.PrescaleOption = TIM_PRESCALE_USVAL;
+	config.PrescaleValue = 100000;
+
+	/*//count control register
+	config_counter.CounterOption = TIM_COUNTER_INCAP0;
+	config_counter.CountInputSelect = 0;*/
+	//CAP1.0 -> pin1.18
+
+	//capture control register
+	config_capture.CaptureChannel = 0;
+	config_capture.RisingEdge = ENABLE;
+	config_capture.FallingEdge = DISABLE;
+	config_capture.IntOnCaption = DISABLE;
+
+	TIM_Init(LPC_TIM1, TIM_COUNTER_RISING_MODE, config);
+	//TIM_ConfigStructInit(TIM_COUNTER_RISING_MODE, config_counter);
+	TIM_ConfigCapture(LPC_TIM1, config_capture);
+	TIM_Cmd(LPC_TIM1, ENABLE);
 
 }
+
+
+
