2dd4d64760d8ed8d22c7aea19c6c40742b5c63a2
Sofia A <sofiamallo07@hotmail.com>
2021-11-03 08:25:15 -0300

    teclado

--- src/TP_Integrador.c
+++ src/TP_Integrador.c
@@ -8,281 +8,197 @@
 ===============================================================================
 */
 
+// Librerías a utilizar
 #include "lpc17xx.h"
-//#include "lpc17xx_timer.h"
 #include "lpc17xx_pinsel.h"
 #include "lpc17xx_gpio.h"
-#include "lpc17xx_pwm.h"
 
+// Definiciones útiles
 #define INPUT 0
 #define OUTPUT 1
+#define RISING 0
+#define FALLING 1
 #define LOWER 0
 #define UPPER 1
 #define PORT(x) x
 #define BIT(x) (1 << x)
 #define ROWS 4
 #define COLUMNS 4
-#define SIZE 10
-#define IN_TECLADO 0xF
-#define OUT_TECLADO 0xF0
+#define SIZE (ROWS * COLUMNS)
+#define TIMER 600000
 
-void configUART();
-void configCapture();
-void configDMA();
-void configGPIO(uint8_t, uint32_t, uint8_t);
-void configPWM();
-void configADC();
-uint8_t get_TeclaMatricial(void);
+// Prototipado de funciones
+void cfg_gpio(void);
+void delay(void);
+void EINT3_IRQHandler(void);
 
-uint8_t pulsaciones;
-uint8_t ppm; //pulsaciones por minuto
-uint8_t buffer[10];
-uint8_t resultado;
-uint8_t acum;
-uint8_t index;
-uint8_t flag;
-uint8_t key;
-uint8_t tecla;
+// Variables globales
+uint32_t p2aux = 0; // Copia auxiliar de la lectura del puerto 2 para antirrebote
 
-uint8_t keys[16] = { 0x06, 0x5b, 0x4f, 0x77,
-		0x66, 0x6d, 0x7d, 0x7c, 0x07, 0x7f, 0x67, 0x39,'*',
-		0x3f, '#', 0x5e
+uint8_t keys[SIZE] = // Teclas del teclado matricial
+{
+	0x06, 0x5b, 0x4f, 0x77, // 1 2 3 A
+	0x66, 0x6d, 0x7d, 0x7c, // 4 5 6 B
+	0x07, 0x7f, 0x67, 0x39, // 7 8 9 C
+	0x79, 0x3f, 0x71, 0x5E  // E 0 F D
 };
 
-int main(void) {
-//---- inicializo todo en 0
-	/*acum = 0;
-	resultado = 0;
-	ppm = 0;
-	index = 0;
-	flag = 0;
-	for(int i = 0; i<10; i++){
-		buffer[i] = 0;
-	}*/
-//-------------------------
-	key = 0;
-	tecla = 0;
-
-	for (uint8_t i = 0; i < 4; i++) // Filas (P2.0-3) como input
-		configGPIO(PORT(2), BIT(i), INPUT);
-
-	for (uint8_t i = 4; i < 8; i++) // Columnas (P2.4-7) como output
-		configGPIO(PORT(2), BIT(i), OUTPUT);
-
-	for (uint8_t i = 0; i < 4; i++)
-	{
-		GPIO_IntCmd(PORT(2), BIT(i), 0);
-		GPIO_ClearInt(PORT(2), BIT(i));
-	}
-
-	for (uint8_t i = 0; i < 7; i++) // Outputs para display 7 segmentos (P2.0-6)
-		configGPIO(PORT(0), BIT(i), OUTPUT);
-
-	NVIC_EnableIRQ(EINT3_IRQn);
+/**
+ * @brief Función principal. Acá se configuran
+ * 		  todos los periféricos.
+ */
+int main(void)
+{
+	cfg_gpio();
 
 	while (1);
 
-    return 0 ;
+    return 0;
 }
-/*
-void configCapture(){
-	//100Mhz por default
-	//queremos que el timer funcione con una precision de 0,1 seg
-	//configurar el prescaler para 0.1 seg = 100.000 useg
 
-	TIM_TIMERCFG_Type config;
-	//TIM_COUNTERCFG_Type config_counter;
-	TIM_CAPTURECFG_Type config_capture;
-
-	config.PrescaleOption = TIM_PRESCALE_USVAL;
-	config.PrescaleValue = 100000;
-
-	/*count control register
-	config_counter.CounterOption = TIM_COUNTER_INCAP0;
-	config_counter.CountInputSelect = 0;*/
-	//CAP1.0 -> pin1.18
-
-	//capture control register
-/*
-	config_capture.CaptureChannel = 0;
-	config_capture.RisingEdge = ENABLE;
-	config_capture.FallingEdge = DISABLE;
-	config_capture.IntOnCaption = ENABLE;
-
-	TIM_Init(LPC_TIM1, TIM_COUNTER_RISING_MODE, &config);
-	//TIM_ConfigStructInit(TIM_COUNTER_RISING_MODE, config_counter);
-	TIM_ConfigCapture(LPC_TIM1, &config_capture);
-	TIM_Cmd(LPC_TIM1, ENABLE);
-
-//----------------------- END OF COUNTER CONFIG---------------------
-
-	TIM_MATCHCFG_Type config_match;
-	config_match.MatchChannel = 0;
-	config_match.IntOnMatch = ENABLE;
-	config_match.StopOnMatch = DISABLE;
-	config_match.ResetOnMatch = ENABLE;
-	config_match.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
-	config_match.MatchValue = 100;
-	//con un PR que se incrementa cada 0.1seg*100 = hace match cada 10 segundos
-
-	TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &config);
-	TIM_ConfigMatch(LPC_TIM0, &config_match);
-	TIM_Cmd(LPC_TIM0, ENABLE);
-
-//----------------------- END OF TIMER CONFIG -------------------------
-
-	NVIC_EnableIRQ(TIMER0_IRQn);
-	NIVC_EnableIRQ(TIMER1_IRQn);
-	NVIC_SetPriority(TIMER0_IRQn, 5);
-	NVIC_SetPriority(TIMER1_IRQn, 10);
-//tiene mas prioridad la interrupcion por match que por capture
-}*/
-
-void configGPIO(uint8_t port, uint32_t pin, uint8_t dir)
+/**
+ * @brief En esta función se configuran
+ * 		  los puertos GPIO a utilizar.
+ *
+ * @details Se configuran P0.0-6 como outputs para
+ * 			mostrar el valor de la tecla presionada.
+ * 			Se configuran P2.0-3 como outputs representando
+ * 			las filas del teclado matricial.
+ * 			Se configuran P2.4-7 como inputs representando
+ * 			las columnas del teclado matricial.
+ */
+void cfg_gpio(void)
 {
 	PINSEL_CFG_Type cfg;
 
-	cfg.Portnum = port;
-	cfg.Pinnum = pin;
-	cfg.Funcnum = 0; // GPIO
+	/****************************************
+	 *								        *
+	 *        CONFIGURACIÓN PUERTO 0        *
+	 *							        	*
+	 ****************************************/
+	cfg.Portnum = PORT(0);
+	cfg.Funcnum = 0; // Función de GPIO
+	cfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+	cfg.Pinmode = PINSEL_PINMODE_TRISTATE;
 
-	if (port == 2)
-		cfg.Pinmode = PINSEL_PINMODE_PULLDOWN;
-
-	PINSEL_ConfigPin(&cfg);
-
-	GPIO_SetDir(port, pin, dir);
-
-	if (dir == OUTPUT && port == 2)
-		GPIO_SetValue(port, pin);
-}
-
-uint8_t get_TeclaMatricial(void)
-{
-	uint8_t fila = 0;
-	uint8_t columna = 0;
-
-	for (columna = 4; columna < 8; columna++)
+	for (uint8_t i = 0; i < 8; i++)
 	{
-		LPC_GPIO0->FIOPIN0 = (0x01<<columna);
+		cfg.Pinnum = i;
 
-		for (fila = 0; fila < 4; fila++)
-		{
-			if (GPIO_ReadValue(PORT(2)) & BIT(fila))
-				return (4*fila + columna);
-		}
+		PINSEL_ConfigPin(&cfg);
 	}
 
-	return (4*fila + columna);
-}
-/*
-void TIMER1_IRQHandler(){ //interrupcion por evento en CAP1.0
-	pulsaciones++;
-	TIM_ClearIntCapturePending(LPC_TIM1, TIM_CR0_INT);
-}
+	GPIO_SetDir(PORT(0), 0xff, OUTPUT);
+	GPIO_ClearValue(PORT(0), 0xff);
 
-void TIMER0_IRQHandler(){ //interrupcion cada 10 segundos
-	uint8_t aux;
+	/****************************************
+	 *								        *
+	 *        CONFIGURACIÓN PUERTO 2        *
+	 *							        	*
+	 ****************************************/
+	cfg.Portnum = PORT(2);
 
-	//lo que hizo en 10 segundos multiplicado por 6, dividido 60
-	aux = (pulsaciones*6);
-	ppm = aux/60;
+	for (uint8_t i = 0; i < 4; i++) // Filas
+	{
+		cfg.Pinnum = i;
 
-	buff[index] = ppm;
-
-	if(index == SIZE){
-		index = 0;
-		if(flag == 0){
-			flag = 1; //completa la primera vuelta
-		}
-	}else index++;
-
-	for(i = 0; i < SIZE; i++){
-		acum += buff[i];
+		PINSEL_ConfigPin(&cfg);
 	}
 
-	if(flag == 0) resultado = acum/index;
-	else resultado = acum/SIZE;
+	cfg.Pinmode = PINSEL_PINMODE_PULLUP;
 
-	//almaceno resultado con DMA
-	acum = 0;
-	pulsaciones = 0;
-}*/
+	for (uint8_t i = 4; i < 8; i++) // Columnas
+	{
+		cfg.Pinnum = i;
+
+		PINSEL_ConfigPin(&cfg);
+	}
+
+	GPIO_SetDir(PORT(2), 0xf, OUTPUT);
+	GPIO_SetDir(PORT(2), (0xf << 4), INPUT);
+
+	// Habilitación de interrupciones por GPIO2
+	FIO_IntCmd(PORT(2), (0xf << 4), FALLING);
+	FIO_ClearInt(PORT(2), (0xf << 4));
+
+	NVIC_EnableIRQ(EINT3_IRQn);
+}
 
 /**
- * @brief Este handler detecta la tecla presionada en el teclado matricial
- * 		  cada vez que ocurre una interrupción por P0.4-7.
+ * @brief Handler para las interrupciones por teclado matricial.
  *
- * @details Se pone en 0 cada fila y se testea cuál columna pasa a 0.
- * 			Ante la primer coincidencia, se le asigna a la variable 'key'
- * 			el valor tomado de la matriz de teclas y se deja de buscar.
+ * @details Se deshabilitan las interrupciones por EINT3 y se
+ * 			almacena una copia de la lectura del puerto 2.
+ * 			Se implementa un antirrebote generado por software.
+ * 			Si luego del retardo por software la lectura del
+ * 			puerto 2 es la misma (la tecla sigue presionada), se
+ * 			interpreta como una pulsación válida.
+ * 			Para obtener las filas, se envía un '1' por cada fila
+ * 			y se analiza el nibble superior del primer byte del
+ * 			puerto 2. Si la fila es la correcta, el '1' debería
+ * 			llegar y todas las columnas deberían estar en '1',
+ * 			por lo que si el nibble superior del primer byte
+ * 			es 0xf, estamos en la fila correcta.
+ * 			Para obtener la columna, recorremos la copia almacenada
+ * 			hasta encontrar un '0' en alguno de los bits del nibble
+ * 			inferior del primer byte.
+ * 			Teniendo la fila y la columna, se muestra por el display
+ * 			el valor de la tecla resultante mediante la ecuación:
+ * 			Key = 4*Row + Column.
+ * 			Finalmente, se limpian las flags de interrupciones y se
+ * 			rehabilitan las interrupciones por EINT3.
  */
 void EINT3_IRQHandler(void)
 {
-	/*uint8_t found = 0;
-	uint8_t column = 0;
+	NVIC_DisableIRQ(EINT3_IRQn);
 
+	p2aux = GPIO_ReadValue(PORT(2)) & 0xf0;
 
-	for (uint8_t row = 0; row < ROWS; row++) // Recorro filas
+	delay();
+
+	if (((GPIO_ReadValue(PORT(2)) & 0xf0) - p2aux) == 0)
 	{
-		if (!found)
+		uint8_t row = 0;
+
+		for (uint8_t i = 0; i < ROWS; i++)
 		{
-			GPIO_ClearValue(PORT(0), BIT(row)); // Fila a 0
+			GPIO_SetValue(PORT(2), BIT(i));
 
-			for (uint8_t col = 0; col < COLUMNS; col++) // Recorro columnas
-				if (!(GPIO_ReadValue(PORT(0)) & BIT(4 + col)))
-				{
-					key = keys[row][col]; // Almaceno el valor cuando hallé coincidencia
+			if ((FIO_ByteReadValue(PORT(2), 0) & 0xf0) == 0xf0)
+			{
+				row = i;
 
-					found = 1;
+				GPIO_ClearValue(PORT(2), BIT(i));
 
-					column = col;
+				break;
+			}
 
-					break;
-					// ABER ESTE COMMIT
-				}
-
-			GPIO_SetValue(PORT(0), BIT(row)); // Fila a 1
+			GPIO_ClearValue(PORT(2), BIT(i));
 		}
-		else
-			break;
-	}*/
-	tecla = get_TeclaMatricial();
-	key = keys[tecla];
-	// Muestro 'key' por display 7 segmentos
-	FIO_HalfWordClearValue(PORT(0), LOWER, 0xff);
-	FIO_HalfWordSetValue(PORT(0), LOWER, key);
 
-	for(int i = 0; i < 4; i++){
-		//if(GPIO_GetIntStatus(2, BIT(i), 0) == ENABLE){
-			GPIO_ClearInt(PORT(2), BIT(i));// Limpio flag de interrupción y termino
-			//break;
-		//}
+		uint8_t col = 0;
+
+		for (uint8_t i = 0; i < COLUMNS; i++)
+			if (!(p2aux & BIT((4 + i))))
+			{
+				col = i;
+
+				break;
+			}
+
+		FIO_HalfWordClearValue(PORT(0), LOWER, 0xff);
+		FIO_HalfWordSetValue(PORT(0), LOWER, keys[((4 * row) + col)]);
 	}
+
+	FIO_ClearInt(PORT(2), (0xf << 4));
+
+	NVIC_EnableIRQ(EINT3_IRQn);
 }
 
-/*
-void configPWM(){
-	PWM_TIMERCFG_Type config;
-	config.PrescaleOption = PWM_TIMER_PRESCALE_USVAL;
-	config.PrescaleValue = 1; //1 useg
-	PWM_MATCHCFG_Type match_config1, match_config0;
-	match_config1.IntOnMatch = DISABLE;
-	match_config1.StopOnMatch = DISABLE;
-	match_config1.ResetOnMatch = ENABLE;
-	match_config1.MatchChannel = 1;
-	match_config0.IntOnMatch = DISABLE;
-	match_config0.StopOnMatch = DISABLE;
-	match_config0.ResetOnMatch = ENABLE;
-	match_config0.MatchChannel = 0;
-
-	PWM_Init(LPC_PWM1, PWM_MODE_TIMER, &config);
-	PWM_Cmd(LPC_PWM1, ENABLE);
-	PWM_ConfigMatch(LPC_PWM1, &match_config0);
-	PWM_ConfigMatch(LPC_PWM1, &match_config1);
-	PWM_MatchUpdate(LPC_PWM1, 0, 1000, PWM_MATCH_UPDATE_NOW); //periodo 1ms
-	PWM_MatchUpdate(LPC_PWM1, 1, 250, PWM_MATCH_UPDATE_NOW); //ancho del pulso 250us
-	//PWM_ChannelConfig(LPC_PWM1, 1, PWM_CHANNEL_SINGLE_EDGE);
-	//PWM_ChannelCmd(LPC_PWM1, 0, ENABLE);
-	PWM_ChannelCmd(LPC_PWM1, 1, ENABLE);
-}*/
+/**
+ * @brief Esta función representa un pequeño delay para antirrebote.
+ */
+void delay(void)
+{
+	for (uint32_t i = 0; i < TIMER; i++);
+}
